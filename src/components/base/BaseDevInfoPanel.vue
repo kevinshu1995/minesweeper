2b2d03975f816826262cfe268d2eca8e8f099ea0
kevinhws <kevinshu1995@gmail.com>
2025-05-09 22:25:03 +0800

    refactor(BaseDevInfoPanel): 調整面板顯示邏輯
    - 新增 opacity 和 transition 效果以改善面板顯示
    - 整合 useWindowSize 和 useElementBounding 以獲取面板尺寸
    - 優化拖曳邏輯，確保面板在初始化時正確顯示

--- src/components/base/BaseDevInfoPanel.vue
+++ src/components/base/BaseDevInfoPanel.vue
@@ -1,5 +1,5 @@
 <template>
-    <div ref="elInfoPanel" class="fixed z-10 border border-neutral-100 rounded bg-white/10" :style="style">
+    <div ref="elInfoPanel" class="fixed z-10 border border-neutral-100 rounded bg-white/10" :style="style" :class="[{ 'opacity-0': !displayInfoPanel }, 'transition-opacity duration-300']">
         <div ref="elDraggingHandle" class="cursor-move flex items-center justify-center border-b border-neutral-100 text-neutral-400 py-1">
             <Icon icon="radix-icons:drag-handle-dots-2" class="rotate-90" />
         </div>
@@ -25,8 +25,8 @@
 </template>
 
 <script setup lang="ts">
-import { useDraggable } from "@vueuse/core";
-import { useTemplateRef } from "vue";
+import { useDraggable, useWindowSize, useElementBounding } from "@vueuse/core";
+import { ref, onMounted, nextTick, useTemplateRef } from "vue";
 import { Icon } from "@iconify/vue";
 import { useDevInfoStore } from "@/store/useDevInfo";
 import { storeToRefs } from "pinia";
@@ -36,10 +36,20 @@ const { devInfoContentArray } = storeToRefs(devInfoStore);
 
 const elInfoPanel = useTemplateRef<HTMLElement>("elInfoPanel");
 const elDraggingHandle = useTemplateRef<HTMLElement>("elDraggingHandle");
+const { height: windowHeight, width: windowWidth } = useWindowSize();
+const { width: elInfoPanelWidth } = useElementBounding(elInfoPanel);
+const displayInfoPanel = ref(false);
 
-const { style } = useDraggable(elInfoPanel, {
-    initialValue: { x: 40, y: 40 },
+const { x, y, style } = useDraggable(elInfoPanel, {
     handle: elDraggingHandle,
+});
+
+onMounted(async () => {
+    await nextTick();
+    const offset = 20;
+    y.value = offset;
+    x.value = windowWidth.value - elInfoPanelWidth.value - offset;
+    displayInfoPanel.value = true;
 });
 </script>
 
