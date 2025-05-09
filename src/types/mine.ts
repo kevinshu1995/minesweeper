63aa36a3888f0c2fc39c4a3679fcd7ae37936c76
wenshiu.hsu <wenshiu.hsu@gmail.com>
2025-05-09 12:26:10 +0800

    refactor(mine): 移除 MineGrid 類型定義
    - 刪除 MineGrid 類型定義以簡化代碼
    - 調整 SingleMineBoxAxis 的註解格式

--- src/types/mine.ts
+++ src/types/mine.ts
@@ -19,7 +19,6 @@ export type UpdateFlagTypeReturn =
       };
 
 export type GetBoxType = () => RevealType;
-
 export type UpdateBoxType = () => UpdateBoxTypeReturn;
 export type UpdateFlagType = () => UpdateFlagTypeReturn;
 
@@ -34,38 +33,35 @@ export type SurroundBoxesIndex = {
     bottomLeft: number | null;
 };
 
-export type MineGrid = {
-    axis: {
-        x: number;
-        y: number;
-        index: number;
-        surroundBoxesIndex: SurroundBoxesIndex;
-    };
-    isMine: boolean;
-    isRevealed: boolean;
-    flagType: FlagType;
-    revealType: RevealType;
-    getBoxType: GetBoxType;
-    updateBoxType: UpdateBoxType;
-    updateFlagType: UpdateFlagType;
-}[][];
-
+/** @description 單一格子的座標 */
 export type SingleMineBoxAxis = {
+    /** @description 座標 x (from 0) */
     x: number;
+    /** @description 座標 y (from 0) */
     y: number;
+    /** @description 附近格子的 index (from 0) */
     surroundBoxesIndex: SurroundBoxesIndex;
 };
 
 export type SingleMineBox = {
     axis: SingleMineBoxAxis;
+    /** @description 此格的 index 值 (x + y) */
     index: number;
+    /** @description 此格的顯示的狀態（被使用者看到的狀態） */
     state: RevealType | FlagType;
+    /** @description 此格是否是地雷 */
     isMine: boolean;
+    /** @description 此格是否已被揭露 */
     isRevealed: boolean;
+    /** @description 此格被使用者標記的狀態 */
     flagType: FlagType;
+    /** @description 此格揭露的狀態 */
     revealType: RevealType;
+    /** @description 取得此格的狀態（沒有 side effect） */
     getBoxType: GetBoxType;
+    /** @description 揭露此格（更新 revealType) */
     updateBoxType: UpdateBoxType;
+    /** @description 改變此格的使用者標記的狀態（更新 flagType) */
     updateFlagType: UpdateFlagType;
 };
 
