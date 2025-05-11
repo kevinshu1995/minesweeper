7746086e2ca3b88039e797abdb7e8e60ff3e7ba4
kevinhws <kevinshu1995@gmail.com>
2025-05-12 02:25:33 +0800

    feat(MineSingleTile): 更新地雷單元格顯示邏輯
    - 改善地雷單元格的顯示邏輯，使用過渡效果
    - 新增對於「問題」和「旗標」狀態的顯示
    - 調整按鈕樣式以增強可用性

--- src/components/MineSingleTile.vue
+++ src/components/MineSingleTile.vue
@@ -1,19 +1,31 @@
 <template>
-    <div :class="['size-10 border shadow m-1 text-5 rounded-lg transition-all duration-100', tileClass]" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
-        <button @click="mine.revealTile" @contextmenu.prevent="mine.updateFlagType" class="w-full h-full cursor-pointer flex items-center justify-center">
-            <template v-if="isZero"> </template>
-            <template v-else-if="mine.revealState === 'question'">
-                <Icon icon="material-symbols:indeterminate-question-box-rounded" class="text-5.5" />
-            </template>
-            <template v-else-if="mine.revealState === 'flag'">
-                <Icon icon="material-symbols:flag-rounded" class="text-5.5" />
-            </template>
-            <template v-else-if="mine.revealState === 'mine'">
+    <div :class="['size-10 m-1 text-5']" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+        <button
+            @click="mine.revealTile"
+            @contextmenu.prevent="mine.updateFlagType"
+            :class="['rounded-lg transition-all duration-100 shadow overflow-hidden w-full h-full border flex items-center justify-center relative', tileClass]"
+        >
+            <template v-if="mine.revealState === 'mine'">
                 <Icon icon="material-symbols:skull" class="text-5.5" />
             </template>
-            <template v-else>
+            <template v-if="typeof mine.revealState === 'number' && mine.revealState !== 0">
                 {{ mine.revealState }}
             </template>
+            <TransitionGroup
+                enter-active-class="transform-gpu transition origin-bottom duration-200 ease-out"
+                leave-active-class="transform-gpu transition origin-bottom duration-200 ease-out"
+                enter-from-class="opacity-0 rotate-60"
+                enter-to-class="opacity-100 rotate-0"
+                leave-from-class="opacity-100 rotate-0"
+                leave-to-class="opacity-0 -rotate-60"
+            >
+                <div v-show="mine.revealState === 'question'" key="question" class="size-full absolute left-0 top-0 flex items-center justify-center">
+                    <Icon icon="material-symbols:indeterminate-question-box-rounded" class="text-5.5" />
+                </div>
+                <div v-show="mine.revealState === 'flag'" key="flag" class="size-full absolute left-0 top-0 flex items-center justify-center">
+                    <Icon icon="material-symbols:flag-rounded" class="text-5.5" />
+                </div>
+            </TransitionGroup>
             <div v-if="mine.isMine" class="opacity-20 absolute inset-0">💀</div>
         </button>
     </div>
@@ -42,16 +54,19 @@ const tileClass = computed(() => {
     if (isZero.value) {
         return "bg-[#BBD8A3]/30 border-[#BBD8A3]";
     }
+    if (typeof props.mine.revealState === "number") {
+        return "bg-[#BBD8A3]/30 border-[#BBD8A3]";
+    }
     if (props.mine.revealState === "question") {
-        return "border-[#E16A54]/50 text-[#E16A54]";
+        return "border-[#E16A54]/50 text-[#E16A54] hover:scale-110 cursor-pointer";
     }
     if (props.mine.revealState === "flag") {
-        return "border-[#BF9264]/50 text-[#BF9264]";
+        return "border-[#BF9264]/50 text-[#BF9264] hover:scale-110 cursor-pointer";
     }
     if (props.mine.revealState === "mine") {
         return "bg-black/80 border-black/50 text-white";
     }
-    return "bg-white/10 border-neutral-300";
+    return "bg-white/10 border-neutral-300 hover:bg-white/50 hover:scale-110 cursor-pointer";
 });
 
 function formatContent<T>(value: T) {
