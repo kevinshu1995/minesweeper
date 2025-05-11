89fd1a28258f1eb196ffeb32dae849c5f4a73a1d
kevinhws <kevinshu1995@gmail.com>
2025-05-12 00:39:08 +0800

    feat(MineSingleTile): 更新地雷單元格顯示邏輯
    - 重構單元格顯示邏輯，使用圖示替代文字顯示
    - 增加不同狀態的圖示：問題、旗標、地雷
    - 調整樣式以增強視覺效果

--- src/components/MineSingleTile.vue
+++ src/components/MineSingleTile.vue
@@ -1,17 +1,30 @@
 <template>
-    <div class="size-10 border border-neutral-200 m-px" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
-        <button @click="mine.revealTile" @contextmenu.prevent="mine.updateFlagType" class="w-full h-full cursor-pointer">
-            {{ mine.revealState }}
-            <div v-if="mine.isMine">💀</div>
+    <div :class="['size-10 border shadow m-1 text-5 rounded-lg transition-all duration-100', tileClass]" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+        <button @click="mine.revealTile" @contextmenu.prevent="mine.updateFlagType" class="w-full h-full cursor-pointer flex items-center justify-center">
+            <template v-if="isZero"> </template>
+            <template v-else-if="mine.revealState === 'question'">
+                <Icon icon="material-symbols:indeterminate-question-box-rounded" class="text-5.5" />
+            </template>
+            <template v-else-if="mine.revealState === 'flag'">
+                <Icon icon="material-symbols:flag-rounded" class="text-5.5" />
+            </template>
+            <template v-else-if="mine.revealState === 'mine'">
+                <Icon icon="material-symbols:skull" class="text-5.5" />
+            </template>
+            <template v-else>
+                {{ mine.revealState }}
+            </template>
+            <div v-if="mine.isMine" class="opacity-20 absolute inset-0">💀</div>
         </button>
     </div>
 </template>
 
 <script setup lang="ts">
+import { Icon } from "@iconify/vue";
 import type { UseMineReturn } from "@/composables/useMineSet";
 import { useDevInfoStore } from "@/store/useDevInfo";
 // import { storeToRefs } from "pinia";
-import { toRaw, toValue } from "vue";
+import { computed, toRaw, toValue } from "vue";
 
 const devInfoStore = useDevInfoStore();
 // const { devInfoContentArray } = storeToRefs(devInfoStore);
@@ -20,6 +33,26 @@ const props = defineProps<{
     mine: NonNullable<UseMineReturn["mineSets"]["value"]>[number][number];
     size: number;
 }>();
+
+const isZero = computed(() => {
+    return props.mine.revealState === 0;
+});
+
+const tileClass = computed(() => {
+    if (isZero.value) {
+        return "bg-[#BBD8A3]/30 border-[#BBD8A3]";
+    }
+    if (props.mine.revealState === "question") {
+        return "border-[#E16A54]/50 text-[#E16A54]";
+    }
+    if (props.mine.revealState === "flag") {
+        return "border-[#BF9264]/50 text-[#BF9264]";
+    }
+    if (props.mine.revealState === "mine") {
+        return "bg-black/80 border-black/50 text-white";
+    }
+    return "bg-white/10 border-neutral-300";
+});
 
 function formatContent<T>(value: T) {
     if (value === null) {
